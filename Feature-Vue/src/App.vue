9649bb7d2bf8b843be87e12fcb00d197799f438c
sito44 <andreesito@gmail.com>
2024-08-01 20:44:41 -0700

    Vue initialization

---
+++ Feature-Vue/src/App.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Footer from './components/Footer.vue'
+
+// export default {
+//   data() {
+//     return {
+//       logolocation: logoImg
+//     }
+//   },
+//   components: {
+//     Logo,
+//     Footer
+//   }
+// }
+</script>
+
+<template>
+  <div id="app">
+    <header id="navBar">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+      <nav class="navigation">
+        <RouterLink class="route-link" to="/">Home</RouterLink>
+        <RouterLink class="route-link" to="/about">About</RouterLink>
+      </nav>
+    </header>
+    <main>
+      <RouterView />
+    </main>
+    <Footer class="flex-style">
+      <ul class="flex-box">
+        <li><a href="https://papito-paul.blogspot.com/" target="_blank">papito-paul.blogspot.com</a></li>
+        <li><a href="https://github.com/JustinTienken-Harder" target="_blank">Github: JustinTienken-Harder</a></li>
+        <li><a href="https://github.com/sito44" target="_blank">Github: sito44</a></li>
+      </ul>
+    </Footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import './assets/mixins.scss';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: rgb(98, 113, 111);
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  overflow-x: hidden;
+  font-size: 62.5%;
+  /* background-image: url('./assets/patterns/escheresque.png'); */
+  scroll-behavior: smooth;
+}
+#navBar {
+  z-index: 500;
+  background: linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.4));
+  background-color: rgba(white, 0.8);
+  width: 100%;
+  padding: 0 0 0 0.3rem;
+  box-shadow: 0 -1px 5px black;
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  .navigation {
+    align-self: center;
+    flex: 1 0 auto;
+    padding-right: 1rem;
+    text-align: right;
+  }
+  .route-link {
+    transition: all 0.2s ease;
+    font-weight: bold;
+    color: #2c3e50;
+    font-size: 1rem;
+    padding: 0 0.8rem 0 0.8rem;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+main {
+  margin-bottom: 10.8rem;
+  @include respond(tab-land) {
+    margin-bottom: 0;
+  }
+  @include respond(tab-port) {
+    margin-bottom: 0;
+  }
+  @include respond(phone) {
+    margin-bottom: 0;
+  }
+}
+
+.flex-style {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+
+</style>
---
+++ Feature-Vue/src/components/Footer.vue
@@ -0,0 +1,67 @@
+<template>
+    <footer>
+        <h1>Sources:</h1>
+        <hr>
+        <slot />
+    </footer>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss">
+@import '../assets/mixins.scss';
+    footer {
+        position: absolute;
+        bottom:0;
+        width: 100vw;
+        background-color: #2c3e50;
+        color: white;
+        @include respond(tab-land) {
+            display: none;
+        }
+        @include respond(tab-port) {
+            display: none;
+        }
+        @include respond(phone) {
+            display: none;
+            
+        }
+        h1{
+            position: absolute;
+            top:.2rem;
+            left:3.5rem;
+        }
+        hr {
+            position: absolute;
+            top:2.6rem;
+            left:3.5rem;
+            color: white;
+            width:78vw;
+            margin: 0 auto;
+            font-size: .01rem;
+        @include respond(big-desktop){
+            width:54vw;
+        }
+        @include respond(desktop) {
+           width:80vw;
+        }
+        }
+        ul {
+            margin-top: 3rem;
+            li {
+                list-style-type: none;
+                text-align: left;
+                margin:1rem;
+                a {
+                    color: white;
+                    text-decoration: none;
+                    font-size: .9rem;
+                }
+            }
+        }
+    }
+</style>
