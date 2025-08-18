98bef17d30e752fbda6a69a69754eaa3183b8912
sito44 <andreesito@gmail.com>
2025-08-17 21:31:02 -0700

    added gh-pages package for deploying vue to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: 'https://justintienken-harder.github.io/papito'
+  base: '/papito/'
 })
 
